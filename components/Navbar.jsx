c864bf367ec8e85612e7b6a5bc8b7c49e37efba8
Rajvardhan <singhrajvardhan2003@gmail.com>
2023-03-29 19:47:28 +0530

    some fixed for deployment

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,9 +1,9 @@
 'use client';
 
-import { motion } from "framer-motion";
+import { motion } from 'framer-motion';
 
-import styles from "../styles";
-import { navVariants } from "../utils/motion";
+import styles from '../styles';
+import { navVariants } from '../utils/motion';
 
 const Navbar = () => (
   <motion.nav
